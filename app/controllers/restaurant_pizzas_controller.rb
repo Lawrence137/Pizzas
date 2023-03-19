ab69e3ac6437ecffddadfdaf16d1d8e716f20e61
Lawrence Kimani <lawrencekimani18@gmail.com>
2023-03-19 12:13:34 +0300

    created restaurantPizza create action

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,22 @@
 class RestaurantPizzasController < ApplicationController
+    # def index
+    #     restaurant_pizzas = RestaurantPizza.all
+    #     render json: restaurant_pizzas
+    # end
+    
+    def create
+        restaurant_pizza = RestaurantPizza.create(restaurant_pizza_params)
+
+        if restaurant_pizza
+            render json: Pizza.find(restaurant_pizza_params[:pizza_id]), only: [:id, :name, :ingredients]
+        else
+            render json: { errors: restaurant_pizza.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+        private 
+
+    def restaurant_pizza_params
+        params.require(:restaurant_pizza).permit(:price, :pizza_id, :restaurant_id)
+    end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,7 +7,7 @@ class RestaurantsController < ApplicationController
     def show
         restaurant = Restaurant.includes(:pizzas).find_by(id: params[:id])
         if restaurant
-            render json: restaurant.as_json(except: [:created_at, :updated_at], include: { pizzas: { only: [:id, :name, :ingredients] } })
+            render json: restaurant.as_json(except: [:created_at, :updated_at], include: { pizzas: { except: [:created_at, :updated_at] } })
 
         else
           render json: { error: "Restaurant not found" }, status: :not_found
