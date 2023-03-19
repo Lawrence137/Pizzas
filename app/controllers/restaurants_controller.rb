00e3f99253349e79a4731770100195d8268cb0d8
Lawrence Kimani <lawrencekimani18@gmail.com>
2023-03-19 09:40:05 +0300

    fetch restaurant by id along with associated pizzas

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,14 @@ class RestaurantsController < ApplicationController
         restaurants = Restaurant.all
         render json: restaurants, except: [:created_at, :updated_at]
     end
+
+    def show
+        restaurant = Restaurant.includes(:pizzas).find_by(id: params[:id])
+        if restaurant
+            render json: restaurant.as_json(except: [:created_at, :updated_at], include: { pizzas: { only: [:id, :name, :ingredients] } })
+
+        else
+          render json: { error: "Restaurant not found" }, status: :not_found
+        end
+    end
 end
