208217c54b5760f684e77db5995bd84cbd57e22c
Lawrence Kimani <lawrencekimani18@gmail.com>
2023-03-19 09:21:53 +0300

    render all restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,6 @@
 class RestaurantsController < ApplicationController
+    def index
+        restaurants = Restaurant.all
+        render json: restaurants, except: [:created_at, :updated_at]
+    end
 end
